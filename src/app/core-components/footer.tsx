c1b953ebde2d2677eeff8551fbdb0f6cc1d89b54
nicholas-0101 <nsvertian@gmail.com>
2025-08-24 22:42:32 +0700

    change follow us logo in footer (#5)

--- src/app/core-components/footer.tsx
+++ src/app/core-components/footer.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="w-full text-neutral-600 pb-10">
       <hr className="my-8 border-t-2 border-gray-600 w-full mx-0" />
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-8">
-        {/* Left: Logo & description */}
+       
         <div>
           <Link href={"/"}>
             <img
@@ -21,7 +22,6 @@ export default function Footer() {
           </p>
         </div>
 
-        {/* Middle: Quick Links */}
         <div>
           <h3 className="text-lg font-semibold mb-3">Quick Links</h3>
           <ul className="space-y-2 text-sm">
@@ -48,18 +48,17 @@ export default function Footer() {
           </ul>
         </div>
 
-        {/* Right: Follow Us */}
         <div className="flex flex-col md:items-end">
           <h3 className="text-lg font-semibold mb-3">Follow Us</h3>
           <div className="flex gap-4">
             <a href="#" className="hover:text-[#00481a]">
-              Facebook
+              <FaFacebook size={32}/>
             </a>
             <a href="#" className="hover:text-[#00481a]">
-              Instagram
+              <FaInstagram size={32}/>
             </a>
             <a href="#" className="hover:text-[#00481a]">
-              Twitter
+              <FaTwitter size={32}/>
             </a>
           </div>
         </div>
