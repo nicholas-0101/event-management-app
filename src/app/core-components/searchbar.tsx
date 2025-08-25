78665ac9e2228515199774bc8b9a2e21027569d4
nicholas-0101 <nsvertian@gmail.com>
2025-08-25 23:02:53 +0700

    create search function and make constant aspect ratio for banner (#8)

--- src/app/core-components/searchbar.tsx
+++ src/app/core-components/searchbar.tsx
@@ -1,12 +1,46 @@
+// import { Input } from "@/components/ui/input";
+// import { Search } from "lucide-react";
+
+// export default function SearchBar() {
+//   return (
+//     <div className="w-4xl h-15 rounded-full shadow-md hover:shadow-lg transition shadow-gray-300/50 border border-gray-200 bg-white px-4 flex gap-1 items-center">
+//       <Search color="#4E7C05"/>
+//       <Input
+//         placeholder="Search events, artists, teams, and more"
+//         className="border-0 focus-visible:ring-0 focus-visible:ring-offset-0 bg-transparent shadow-none"
+//       />
+//     </div>
+//   );
+// }
+
+"use client";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
+import { useState, useEffect } from "react";
 
-export default function SearchBar() {
+type SearchBarProps = {
+  onSearch: (query: string) => void;
+  delay?: number;
+};
+
+export default function SearchBar({ onSearch, delay = 500 }: SearchBarProps) {
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      onSearch(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay, onSearch]);
+
   return (
     <div className="w-4xl h-15 rounded-full shadow-md hover:shadow-lg transition shadow-gray-300/50 border border-gray-200 bg-white px-4 flex gap-1 items-center">
-      <Search color="#4E7C05"/>
+      <Search color="#4E7C05" />
       <Input
         placeholder="Search events, artists, teams, and more"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         className="border-0 focus-visible:ring-0 focus-visible:ring-offset-0 bg-transparent shadow-none"
       />
     </div>
