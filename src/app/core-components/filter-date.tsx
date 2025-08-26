4864c4beb240dd88a59e21de16e787eef7a89fc8
nicholas-0101 <nsvertian@gmail.com>
2025-08-26 14:02:53 +0700

    make create event ui and validation, update filter date

--- src/app/core-components/filter-date.tsx
+++ src/app/core-components/filter-date.tsx
@@ -1,6 +1,64 @@
+// "use client";
+// import * as React from "react";
+// import { CalendarDays, ChevronDownIcon } from "lucide-react";
+// import { Button } from "@/components/ui/button";
+// import { Calendar } from "@/components/ui/calendar";
+// import {
+//   Popover,
+//   PopoverContent,
+//   PopoverTrigger,
+// } from "@/components/ui/popover";
+
+// export default function DateFilter() {
+//   const [open, setOpen] = React.useState(false);
+//   const [date, setDate] = React.useState<Date | undefined>(undefined);
+
+//   return (
+//     <div className="flex flex-col gap-3">
+//       <Popover open={open} onOpenChange={setOpen}>
+//         <PopoverTrigger asChild>
+//           <Button
+//             variant="outline"
+//             id="date"
+//             className={`w-48 justify-between font-medium rounded-full cursor-pointer border-1 border-[#00481a] text-[#00481a] transition-colors
+//               ${
+//                 date
+//                   ? "bg-[#c6ee9a] hover:bg-[#97d753]"
+//                   : "bg-[#97d753] hover:bg-[#c6ee9a]"
+//               }
+//             `}
+//           >
+//             <CalendarDays className="mr-2 h-4 w-4" />
+//             {date ? date.toLocaleDateString() : "Select date"}
+//             <ChevronDownIcon className="ml-2 h-4 w-4" />
+//           </Button>
+//         </PopoverTrigger>
+//         <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+//           <Calendar
+//             mode="single"
+//             selected={date}
+//             captionLayout="dropdown"
+//             onSelect={(date) => {
+//               setDate(date);
+//               setOpen(false);
+//             }}
+//             disabled={(date) => {
+//               const today = new Date();
+//               const yesterday = new Date(today);
+//               yesterday.setDate(today.getDate() - 1);
+//               return date < yesterday; // disable date before yesterday
+//             }}
+//           />
+//         </PopoverContent>
+//       </Popover>
+//     </div>
+//   );
+// }
+
+
 "use client";
-import * as React from "react";
-import { CalendarDays, ChevronDownIcon } from "lucide-react";
+
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
@@ -8,10 +66,13 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { CalendarDays, ChevronDownIcon } from "lucide-react";
+import { DateRange } from "react-day-picker";
+import { format } from "date-fns";
 
-export default function DateFilter() {
-  const [open, setOpen] = React.useState(false);
-  const [date, setDate] = React.useState<Date | undefined>(undefined);
+export default function DateRangePicker() {
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState<DateRange | undefined>();
 
   return (
     <div className="flex flex-col gap-3">
@@ -20,34 +81,41 @@ export default function DateFilter() {
           <Button
             variant="outline"
             id="date"
-            className={`w-48 justify-between font-medium rounded-full cursor-pointer border-1 border-[#00481a] text-[#00481a] transition-colors
+            className={`w-auto justify-between font-medium rounded-full cursor-pointer border border-[#00481a] text-[#00481a] transition-colors
               ${
-                date
+                date?.from && date?.to
                   ? "bg-[#c6ee9a] hover:bg-[#97d753]"
                   : "bg-[#97d753] hover:bg-[#c6ee9a]"
               }
             `}
           >
             <CalendarDays className="mr-2 h-4 w-4" />
-            {date ? date.toLocaleDateString() : "Select date"}
+            {date?.from && date?.to ? (
+              `${format(date.from, "PPP")} - ${format(date.to, "PPP")}`
+            ) : (
+              "Select date range"
+            )}
             <ChevronDownIcon className="ml-2 h-4 w-4" />
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-auto overflow-hidden p-0" align="start">
           <Calendar
-            mode="single"
+            mode="range"
             selected={date}
-            captionLayout="dropdown"
-            onSelect={(date) => {
-              setDate(date);
-              setOpen(false);
+            onSelect={(range) => {
+              setDate(range);
+              if (range?.from && range?.to) {
+                setOpen(true);
+              } 
             }}
+            numberOfMonths={1}
             disabled={(date) => {
               const today = new Date();
               const yesterday = new Date(today);
               yesterday.setDate(today.getDate() - 1);
-              return date < yesterday; // disable date before yesterday
+              return date < yesterday;
             }}
+            className="rounded-md border"
           />
         </PopoverContent>
       </Popover>
---
+++ src/app/eo-event-creation/EventCreationSchema.tsx
@@ -0,0 +1,78 @@
+import * as Yup from "yup";
+
+export interface ITicket {
+  ticket_price: number | null;
+  ticket_quota: number | null;
+  ticket_type: string;
+}
+
+export interface IEventCreate {
+  event_name: string;
+  event_total_seats: number | null;
+  event_description: string;
+  event_location: string;
+  event_category: "concert" | "festival" | "sport" | "theater";
+  event_thumbnail: File | null;
+  event_start_date: Date | string;
+  event_end_date: Date | string;
+  tickets: ITicket[];
+}
+
+export const initialValues: IEventCreate = {
+    event_name: "",
+    event_total_seats: null,
+    event_description: "",
+    event_location: "",
+    event_category: "" as "concert" | "festival" | "sport" | "theater",
+    event_thumbnail: null,
+    event_start_date: "",
+    event_end_date: "",
+    tickets: [
+      {
+        ticket_type: "Regular",
+        ticket_price: null,
+        ticket_quota: null,
+      },
+      {
+        ticket_type: "vip",
+        ticket_price: null,
+        ticket_quota: null,
+      },
+    ],
+  };
+
+export const eventCreationSchema = Yup.object().shape({
+  event_name: Yup.string().required("*event name is required"),
+  event_total_seats: Yup.number()
+    .typeError("*total seats must be a number")
+    .min(1, "*total seats must be at least 1")
+    .required("*total seats is required"),
+  event_description: Yup.string().required("*event description is required"),
+  event_location: Yup.string().required("*event location is required"),
+  event_category: Yup.string()
+    .oneOf(["concert", "festival", "sport", "theater"], "*invalid category")
+    .required("*event category is required"),
+  event_thumbnail: Yup.mixed<File>()
+    .required("*event thumbnail is required")
+    .test("fileType", "*only image files are allowed", (value) => {
+      if (!value) return false;
+      return ["image/jpeg", "image/png", "image/gif", "image/webp"].includes(
+        value.type
+      );
+    }),
+  event_start_date: Yup.date().required("*event date is required"),
+ 
+  tickets: Yup.array()
+    .of(
+      Yup.object().shape({
+        ticket_price: Yup.number()
+          .nullable().min(0, "*price can't negative")
+          .required("*price is required"),
+        ticket_quota: Yup.number()
+          .nullable().min(1, "*quota must be at least 1")
+          .required("*quota is required"),
+        
+      })
+    )
+    .min(1, "*at least one ticket is required"),
+});
