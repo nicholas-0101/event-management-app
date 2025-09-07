94c9ab6dbdeb297f48072644e52908146dc66ca3
nicholas-0101 <nsvertian@gmail.com>
2025-09-07 19:45:26 +0700

    responsive in create event (#50)

--- src/app/event-organizer/event-creation/page.tsx
+++ src/app/event-organizer/event-creation/page.tsx
@@ -253,7 +253,7 @@ export default function EventCreationPage() {
                       }
                     }}
                     numberOfMonths={1}
-                    className="rounded-lg border w-full"
+                    className="rounded-lg border w-69"
                     disabled={(d) => {
                       const today = new Date();
                       const yesterday = new Date(today);
@@ -261,7 +261,7 @@ export default function EventCreationPage() {
                       return d < yesterday;
                     }}
                   />
-                  <div className="flex justify-center items-center text-sm text-gray-600 mt-2 border border-neutral-200 rounded-lg h-10 px-4">
+                  <div className="flex justify-center items-center text-sm text-gray-600 mt-2 border border-neutral-200 rounded-lg h-10 px-4 w-69">
                     {values.event_start_date && values.event_end_date ? (
                       <span className="font-medium">
                         {format(values.event_start_date, "PPP")} -{" "}
@@ -277,7 +277,7 @@ export default function EventCreationPage() {
                     </span>
                   )}
 
-                  <div className="mt-4 border-t pt-4 flex flex-col gap-2">
+                  <div className="mt-4 border-t pt-4 flex flex-col gap-2 w-69">
                     <Label>Tickets (in IDR)</Label>
                     <div className="border border-neutral-200 rounded-lg">
                       {/* Regular Ticket */}
